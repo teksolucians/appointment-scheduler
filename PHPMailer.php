d12a952c103dae8f9c2fedf0a7a20d5e56beebc4
teksolucians <130227000+teksolucians@users.noreply.github.com>
2024-01-03 15:33:18 -0500

    Add files via upload

---
+++ PHPMailer.php
@@ -0,0 +1,64 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;    
+require 'vendor/autoload.php'; 
+//session_start();
+
+
+if (isset($_POST['submit'])){
+  
+        //send email section
+
+ 
+        $business_email = "jennysdoubles@gmail.com";
+     
+        $mail = new PHPMailer(true);
+        try {
+            $mail->SMTPOptions = array('ssl'=>array('verify_peer'=>false,'verify_peer_name'=>false,'allow_self_signed' => true));
+            //Server settings
+            $mail->SMTPDebug = false;//SMTP::DEBUG_SERVER;                   // Enable verbose debug output
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = 'jennysdoubles@gmail.com';                     // SMTP username
+            $mail->Password   = 'tdxofiypzoqlohtq';                               // SECOND SMTP password (New 3.1.24)
+            $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = 587;                          // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+            //Recipients
+            $mail->setFrom("jennysdoubles@gmail.com", "New Appointment Order");
+            $mail->addAddress($business_email);     // Add a recipient
+            
+              
+            
+           $body= "<br> You have just received a new appointment<br>Author: Owen Elwin<br><br><hr>";
+            
+            
+            
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'New Appointment';
+            $mail->Body    = $body;
+            $mail->AltBody = strip_tags($body);
+
+
+            if ($mail->send()){
+                echo"Yay";
+            }
+            else{
+                echo"Nay";
+            }
+        }
+        catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {
+               $mail->ErrorInfo
+        }";
+        
+        
+            }
+
+}
+
+
+?>
